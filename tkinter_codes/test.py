bf23bc70d358fe3a0aa194ed5bed411df65cb4f0
omar115 <omarhasan115@gmail.com>
2021-02-12 03:44:30 +0600

    new files added

--- tkinter_codes/test.py
+++ tkinter_codes/test.py
@@ -1,8 +1,10 @@
-def test(text):
-    input_text = text1.get("1.0", "end")
-    out = translator.translate(input_text, lang_tgt=text)
-    print('translated text is ',out)
-    text2.insert('end', out)
+import re
 
-btn = Button(root, text='arabic', bg='red', fg='black', command=lambda: test('ar'))
-btn.grid(row=1, column=2, padx=10, pady=10)
+x = '''20 Feb 2019
+11:41 am
+'''
+print(x)
+
+yr = re.findall('\d*.?\d+', x)
+print(yr)
+
