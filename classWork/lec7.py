f7a8acaecf7716d16ef482682d7b6e8273d73ed1
omar115 <omarhasan115@gmail.com>
2021-06-26 12:11:58 +0600

    commit

---
+++ classWork/lec7.py
@@ -0,0 +1,11 @@
+# String Manipulation
+
+a = 'The Old Man and The Sea, Famous book, NULL'
+b = a.split(',')
+# print(b)
+book_name = b[0]
+description = b[1]
+date = b[2]
+print(book_name)
+print(description)
+print(date)
