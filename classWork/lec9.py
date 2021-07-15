7a28f747808366f125ba1bc6e968e0a2865ada96
omar115 <omarhasan115@gmail.com>
2021-07-16 03:31:00 +0600

    treeview feature added

--- classWork/lec9.py
+++ classWork/lec9.py
@@ -1,5 +1,7 @@
 from tkinter import *
+from tkinter import ttk
 import sqlite3
+import tkinter
 
 root = Tk()
 
@@ -82,21 +84,69 @@ def delete():
                             # and can show in terminal using print function
 
 
+def update():
+    input_text = txt3.get('1.0','end')
+    print(input_text)
+    output_text = input_text.split(',')     #slice the string given from text box 1
+    print(output_text)
+    # assign the output into different variables
+    book_name = output_text[0]
+    date = output_text[1]
+
+    script = 'UPDATE book SET date = (?) where book_name = (?)' # create a placeholder
+
+    cur.execute(script, (date,book_name))     # adding value to the database
+
+    c.commit()  # saving
+
+    cur.execute('SELECT * FROM book')   # select all data from the database
+
+    # c.commit()  # saving
+
+    print(cur.fetchall())   # using fetchall() you can pop out all the data from the database 
+                            # and can show in terminal using print function
+
+
+def show_all():
+    tree= ttk.Treeview(root, column=("column1", "column2", "column3"), show='headings')
+    tree.heading("#1", text="BOOK NAME")
+    tree.heading("#2", text="AUTHOR NAME")
+    tree.heading("#3", text="DATE OF PUBLICATION")
+    tree.grid(row=0, column=7, pady=20)
+
+    cur.execute('SELECT * FROM book')
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+        tree.insert("",tkinter.END, values=row)
 # text 1 where user will write the book name, author name and description
 txt1 = Text(root, height=5, width=25, font='arial')
-txt1.grid(row=0, column=0)
+txt1.grid(row=0, column=0, padx=10, pady=10)
 
 # button to insert data into the database
-btn = Button(root, text='Add the Book', bd=5, command=add)
-btn.grid(row=1, column=0)
+btn1 = Button(root, text='Add the Book', bd=5, command=add)
+btn1.grid(row=1, column=0, padx=10, pady=10)
 
 # text 1 where user will write the book name, author name and description
 txt2 = Text(root, height=5, width=25, font='arial')
-txt2.grid(row=2, column=0)
+txt2.grid(row=2, column=0, padx=10, pady=10)
 
 # button to insert data into the database
-btn = Button(root, text='Delete the Book', bd=5, command=delete)
-btn.grid(row=3, column=0)
+btn2 = Button(root, text='Delete the Book', bd=5, command=delete)
+btn2.grid(row=3, column=0, padx=10, pady=10)
+
+txt3 = Text(root, height=5, width=25, font='arial')
+txt3.grid(row=4, column=0, padx=10, pady=10)
+
+# button to insert data into the database
+btn3 = Button(root, text='Update the Book', bd=5, command=update)
+btn3.grid(row=5, column=0, padx=10, pady=10)
+
+
+btn4 = Button(root, text='Show Book List', bd=5, command=show_all)
+btn4.grid(row=7, column=0, padx=20, pady=20)
+
+
 
 
 root.mainloop()
