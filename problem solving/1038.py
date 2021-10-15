ccb39ce8fae6fa650ce1afa328d1bcf6fde803dd
omar115 <omarhasan115@gmail.com>
2021-10-15 12:22:43 +0600

    new problem solved

--- problem solving/1038.py
+++ problem solving/1038.py
@@ -1,6 +1,24 @@
 a = input().split(' ')
-x = int(a[0])
-y = int(a[1])
+x = int(a[0])   # code
+y = int(a[1])   # quantity
 
+if x == 1:
+    rst = y * 4.00
+    print('Total: R$ {:.2f}'.format(rst))
 
-## do later
+elif x == 2:
+    rst = y * 4.50
+    print('Total: R$ {:.2f}'.format(rst))
+
+elif x == 3:
+    rst = y * 5.00
+    print('Total: R$ {:.2f}'.format(rst))
+
+elif x == 4:
+    rst = y * 2.00
+    print('Total: R$ {:.2f}'.format(rst))
+
+elif x == 5:
+    rst = y * 1.50
+    print('Total: R$ {:.2f}'.format(rst))
+
