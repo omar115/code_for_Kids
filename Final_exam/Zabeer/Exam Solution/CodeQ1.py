b9ee6c8298e1df743837e474bc606af1acf72cd5
omar115 <omarhasan115@gmail.com>
2021-11-26 21:14:55 +0600

    q-a reviewing process of zaber friday

--- Final_exam/Zabeer/Exam Solution/CodeQ1.py
+++ Final_exam/Zabeer/Exam Solution/CodeQ1.py
@@ -1,5 +1,25 @@
-w = int(input("Pls state the number of hours you have worked everyday this month: "))
-i = 15.00
-y = w*i
-print(y)
-print("is the salary for you this month enjoy! ")
+
+# take inputs from the user
+# id_number = int(input())
+# working_hours = int(input())
+# payment = float(input())
+
+
+# # multiply working hours with payment
+# # generating result
+
+# result = working_hours * payment
+
+# # printing final result
+
+# print(result)
+
+# print(id_number)
+
+
+
+# w = int(input("Pls state tshe number of hours you have worked everyday this month: "))
+# i = 15.00
+# y = w*i
+# print(y)
+# print("is the salary for you this month enjoy! ")
--- Final_exam/Zabeer/Exam Solution/CodeQ3.py
+++ Final_exam/Zabeer/Exam Solution/CodeQ3.py
@@ -4,27 +4,38 @@ c = int(input("Give numbers even or odd: "))
 d = int(input("Give numbers even or odd: "))
 e = int(input("Give numbers even or odd: "))
 
+
+count = 0
+
+
 if a % 2 == 0:
     print("even")
+    count = count + 1
 else:
     print("not even")
 
 if b % 2 == 0:
     print("even")
+    count = count + 1
 else:
     print("not even")
 
 if c % 2 == 0:
     print("even")
+    count = count + 1
 else:
     print("not even")
 
 if d % 2 == 0:
     print("even")
+    count = count + 1
 else:
     print("not even")
 
 if e % 2 == 0:
     print("even")
+    count = count + 1
 else:
     print("not even")
+
+print(count)
